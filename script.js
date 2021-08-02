8a924590bbd1047ab74581a5b85f6a1b0c00a7c7
Bladis <vladis.dev@gmail.com>
2021-08-02 14:46:17 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const nav = document.getElementById('nav');
+const toggleIcon = document.getElementById('toggle-icon');
+const image1 = document.getElementById('img1');
+const image2 = document.getElementById('img2');
+const image3 = document.getElementById('img3');
+const textBox = document.getElementById('text-box');
+
+
+function imageMode(color) {
+    image1.src = `/img/undraw_Active_options_${color}.svg`;
+    image2.src = `/img/undraw_Through_the_desert_${color}.svg`;
+    image3.src = `/img/undraw_Work_from_anywhere_${color}.svg`;
+}
+
+function toggleDarkLighMode(isDark) {
+    nav.style.backgroundColor = isDark ? 'rgb(0 0 0 / 50%)' : 'rgb(255 255 255 / 50%)';
+    textBox.style.backgroundColor = isDark ? 'rgb(255 255 255 / 50%)' : 'rgb(0 0 0 / 50%)';
+    toggleIcon.children[0].textContent = isDark ? 'Dark Mode' : 'Light Mode';
+    isDark ? toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon') : toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+    isDark ? imageMode("dark") : imageMode("light");
+}
+
+
+
+function switchTheme(event) {
+    if (event.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+        toggleDarkLighMode(true);
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+        toggleDarkLighMode(false);
+    }
+}
+
+toggleSwitch.addEventListener("change", switchTheme);
+
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+        toggleDarkLighMode(true);
+    }
+
+}
